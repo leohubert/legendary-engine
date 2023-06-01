9c340898b3115a8aadab802ed3eafd9b3fb474d8
Caroline Sclavon <caroline.sclavon@gmail.com>
2023-06-01 16:51:02 +0200

    Authentification de l'utilisateur

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,25 +3,31 @@ const form = document.querySelector('form');
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
 const errorMessage = document.querySelector('#error-message');
+const bouton = document.querySelector('#submit')
 
 // Écouteur d'événement pour la soumission du formulaire
-form.addEventListener('submit', async (event) => {
+bouton.addEventListener('click', async (event) => {
   event.preventDefault(); // Empêche le rechargement de la page
-
   // Récupération des valeurs des champs email et mot de passe
   const email = emailInput.value;
   const password = passwordInput.value;
 
   // Envoi des données au serveur
   try {
-    const response = await fetch('http://localhost:5678/api-docs/#/default/post_users_login', {
+    const response = await fetch('http://localhost:5678/api/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        
       },
-      body: JSON.stringify({ email, password }),
-    });
+      body: JSON.stringify({
+        "email": "sophie.bluel@test.tld",
+        "password": "s0phie"
+      }),
 
+    });
     if (response.ok) {
       // Connexion réussie
       const data = await response.json();
